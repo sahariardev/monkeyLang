1455d07d890247dce6f5bbc566e10e6b33a1497e
sahariardev <rifatsahariar@gmail.com>
2025-07-16 21:20:35 +0600

    feat: update parser to parse keywords

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,14 @@
 package lexer
 
-import "monkeyLang/token"
+import (
+	"fmt"
+	"monkeyLang/token"
+)
+
+var keywords = map[string]token.TokenType{
+	"fn":  token.FUNCTION,
+	"let": token.LET,
+}
 
 type Lexer struct {
 	input        string
@@ -10,13 +18,57 @@ type Lexer struct {
 }
 
 func New(input string) *Lexer {
-	return &Lexer{
+	l := &Lexer{
 		input: input,
 	}
+
+	l.readChar()
+	return l
 }
 
 func (l *Lexer) NextToken() token.Token {
-	return token.Token{}
+	var tok token.Token
+
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Type = token.EOF
+
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = LookUpIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			println("-----")
+			fmt.Printf("%c\n", l.ch)
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+	l.readChar()
+
+	return tok
 }
 
 func (l *Lexer) readChar() {
@@ -25,7 +77,53 @@ func (l *Lexer) readChar() {
 	} else {
 		l.ch = l.input[l.readPosition]
 	}
-	
+
 	l.pos = l.readPosition
 	l.readPosition++
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\r' || l.ch == '\n' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.pos
+
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.pos]
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.pos
+
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.pos]
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+func LookUpIdent(ident string) token.TokenType {
+	if tokeType, ok := keywords[ident]; ok {
+		return tokeType
+	}
+
+	return token.IDENT
 }
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -18,9 +18,76 @@ func TestNextToken(t *testing.T) {
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
 		{token.RBRACE, "}"},
+		{token.COMMA, ","},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		currentToken := l.NextToken()
+
+		if currentToken.Type != tt.expectedType {
+			t.Fatalf("test[%d], expected=%q, got=%q", i,
+				tt.expectedType, currentToken.Type)
+		}
+
+		if currentToken.Literal != tt.expectedValue {
+			t.Fatalf("test[%d], expected=%s, got=%s", i,
+				tt.expectedValue, currentToken.Literal)
+		}
+	}
+}
+
+func TestNextTokenDetail(t *testing.T) {
+	input := `let five = 5;
+	 let ten = 10;
+	 let add = fn(x, y) {
+		x + y;
+	};
+	let result = add(five, ten);`
+
+	tests := []struct {
+		expectedType  token.TokenType
+		expectedValue string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
 		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
 	}
 
 	l := New(input)
