abde8837e8774d045ecb204109b4f3ad2cf0cf2e
sahariardev <rifatsahariar@gmail.com>
2025-07-23 21:32:56 +0600

    feat: add repl

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"monkeyLang/repl"
+	"os"
+	"os/user"
+)
+
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
 func main() {
+	usr, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
 
+	fmt.Printf("Hello %s this is monkey programming language!\n", usr.Username)
+	fmt.Printf("Feel free to type commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
