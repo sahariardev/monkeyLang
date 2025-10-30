a9ce2f98b36fa928f02079be58fb9e075eb171c5
sahariardev <rifatsahariar@gmail.com>
2025-10-30 20:41:18 +0600

    feat: add token parser

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"monkeyLang/ast"
 	"monkeyLang/lexer"
 	"monkeyLang/token"
+	"strconv"
 )
 
 type (
@@ -40,6 +41,7 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	return p
 }
@@ -173,4 +175,19 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	}
 
 	return stmt
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.currentToken}
+
+	value, err := strconv.ParseInt(p.currentToken.Literal, 0, 64)
+
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.currentToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+	return lit
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -105,6 +105,40 @@ func TestIdentifierExpression(t *testing.T) {
 	if identifier.Value != "foobar" {
 		t.Errorf("identifier.Value does not match. got=%q", identifier.Value)
 	}
+}
+
+func TestIntegerLiteralExpression(t *testing.T) {
+	input := "5"
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserError(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain 1 statements. got=%d", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+
+	if !ok {
+		t.Fatalf("stmt.Expression is not ast.Identifier. got=%T", stmt.Expression)
+	}
+
+	if literal.Value != 5 {
+		t.Errorf("identifier.TokenLiteral does not match. got=%q", literal.Value)
+	}
+
+	if literal.TokenLiteral() != "5" {
+		t.Errorf("identifier.Value does not match. got=%q", literal.TokenLiteral())
+	}
 
 }
 
